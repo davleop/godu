b159b639ad44791ea2f9162566ad20de42596750
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-06-28 23:08:35 -0500

    Started adding options. Not currently working.
    Can't get Cobra to respect flags that I'm adding to the root command. I'll figure it out tomorrow. I like Cobra's structure I just feel like I'm doing something stupid.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
     internal/tui v1.0.0
 	github.com/charmbracelet/bubbletea v0.21.0 // direct
 	github.com/containerd/console v1.0.3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
@@ -14,6 +15,8 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/spf13/cobra v1.5.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,64 @@
 package main
 
 import (
+<<<<<<< Updated upstream
     "log"
 
     tea "github.com/charmbracelet/bubbletea"
     . "internal/tui"
+=======
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	//. "internal/tui"
 )
 
+/*
+	This var sets up the root command and then all other commands. The root command, according to Cobra's structure, is the first thing we hit when we run the program.
+	Imagine it as an automatic constructor that's allowing us to run an instance of this program.
+*/
+var (
+	versFlag bool
+	rootCmd  = &cobra.Command{
+		Use: "put usage example here",
+		//TraverseChildren: true,
+		Short: "This program shows disk usage",
+		Long:  "Put longer version of Short here",
+		Run: func(cmd *cobra.Command, args []string) {
+			v, _ := cmd.Flags().GetBool("versFlag")
+			if v {
+				version()
+			} else {
+				fmt.Println("it brokey")
+			}
+		},
+	}
+>>>>>>> Stashed changes
+)
+
+func version() {
+	fmt.Println("Version goes here")
+}
+
+func init() {
+
+	rootCmd.PersistentFlags().BoolVarP(&versFlag, "version", "v", true, "-v or --version: will output the version of godu currently in use")
+
+}
+
 func main() {
+<<<<<<< Updated upstream
     p := tea.NewProgram(InitialModel(), tea.WithAltScreen())
     if err := p.Start(); err != nil {
         log.Fatal(err)
     }
+=======
+	//p := tea.NewProgram(InitialModel(), tea.WithAltScreen())
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+
+>>>>>>> Stashed changes
 }
