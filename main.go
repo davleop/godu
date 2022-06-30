eebc0b1a5fcd43cccad41e184504a7e44d7b15b5
Slime-Sama <78178911+prescient-slime@users.noreply.github.com>
2022-06-29 20:22:17 -0500

    Fixed version flag and started working on -file flag.

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-<<<<<<< Updated upstream
-    "log"
-
-    tea "github.com/charmbracelet/bubbletea"
-    . "internal/tui"
-=======
 	"fmt"
+	//. "internal/tui"
 	"log"
 
 	"github.com/spf13/cobra"
@@ -18,47 +13,45 @@ import (
 	This var sets up the root command and then all other commands. The root command, according to Cobra's structure, is the first thing we hit when we run the program.
 	Imagine it as an automatic constructor that's allowing us to run an instance of this program.
 */
-var (
-	versFlag bool
-	rootCmd  = &cobra.Command{
-		Use: "put usage example here",
-		//TraverseChildren: true,
-		Short: "This program shows disk usage",
-		Long:  "Put longer version of Short here",
-		Run: func(cmd *cobra.Command, args []string) {
-			v, _ := cmd.Flags().GetBool("versFlag")
-			if v {
-				version()
-			} else {
-				fmt.Println("it brokey")
-			}
-		},
-	}
->>>>>>> Stashed changes
-)
+var rootCmd = &cobra.Command{
+	Use: "put usage example here",
+	//TraverseChildren: true,
+	Short: "This program shows disk usage",
+	Long:  "Put longer version of Short here",
+	Run: func(cmd *cobra.Command, args []string) {
+		v, _ := cmd.Flags().GetBool("version")
+		if v {
+			version()
+		} else {
+			log.Fatal("version unavailable")
+		}
+	},
+}
 
 func version() {
 	fmt.Println("Version goes here")
 }
 
-func init() {
+func file() {
 
-	rootCmd.PersistentFlags().BoolVarP(&versFlag, "version", "v", true, "-v or --version: will output the version of godu currently in use")
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("file", "f", true, "-f [FILE] Loads the given file, which has earlier been created with the -o option. If [FILE] is equivalent to -, the file is read from standard input.")
+	rootCmd.PersistentFlags().BoolP("version", "v", true, "-v or --version: will output the version of godu currently in use")
 
 }
 
 func main() {
-<<<<<<< Updated upstream
-    p := tea.NewProgram(InitialModel(), tea.WithAltScreen())
-    if err := p.Start(); err != nil {
-        log.Fatal(err)
-    }
-=======
+	//commented out to avoid going into tui every time I tested code.
+	/*p := tea.NewProgram(InitialModel(), tea.WithAltScreen())
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}*/
 	//p := tea.NewProgram(InitialModel(), tea.WithAltScreen())
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 
->>>>>>> Stashed changes
 }
