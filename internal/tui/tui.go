4d3f0862bb7d2c73eccf68cc84bfd22626cefde0
david <david@malcompliance.mynetworksettings.com>
2022-07-06 22:00:23 -0400

    viewport implementation

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,31 +1,33 @@
 package tui
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 
-    "github.com/charmbracelet/bubbles/viewport"
-    tea "github.com/charmbracelet/bubbletea"
-    "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 
-    . "internal/du"
+	. "internal/du"
 )
 
+const useHighPerformanceRenderer = false
+
 type Model struct {
-    CurrentDirectory string
-    Cursor  int
-    Subcursor int
-    ShowHidden bool
-    Order string
-    DirectoryFirst bool
-    ShowDiskUsage bool
-    ShowUniqCol bool
-    ModifyTime bool
-    Files []File
-    currentFiles []File
-    Viewport viewport.Model
-    Ready bool
-    Content string
+	CurrentDirectory string
+	Cursor           int
+	Subcursor        int
+	ShowHidden       bool
+	Order            string
+	DirectoryFirst   bool
+	ShowDiskUsage    bool
+	ShowUniqCol      bool
+	ModifyTime       bool
+	Files            []File
+	currentFiles     []File
+	Viewport         viewport.Model
+	Ready            bool
+	Content          string
 }
 
 var (
@@ -43,123 +45,150 @@ var (
 )
 
 func (m Model) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    var (
+	var (
 		cmd  tea.Cmd
 		cmds []tea.Cmd
 	)
 
-    switch msg := msg.(type) {
-    case tea.KeyMsg:
-        switch msg.String() {
-        case "ctrl+c", "q", "esc":
-            // exit
-            return m, tea.Quit
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			// exit
+			return m, tea.Quit
 
-        case "up", "k":
-            // cycle items
-            if m.Cursor > 0 {
-                m.Cursor--
-                m.Subcursor--
-            }
+		case "up", "k":
+			// cycle items
+			if m.Cursor > 0 {
+				m.Cursor--
+				m.Subcursor--
+			}
 
-        case "down", "j":
-            // cycle items
-            if m.Cursor < len(m.currentFiles)-1 {
-                m.Cursor++
-                m.Subcursor++
-            }
+		case "down", "j":
+			// cycle items
+			if m.Cursor < len(m.currentFiles)-1 {
+				m.Cursor++
+				m.Subcursor++
+			}
 
-        case "enter", " ", "l", "right":
-            // open selected directory
+		case "enter", " ", "l", "right":
+			// open selected directory
 
-        case "left", "h":
-            // go to parent directory
+		case "left", "h":
+			// go to parent directory
 
-        case "n":
-            // order by filename
+		case "n":
+			// order by filename
 
-        case "s":
-            // order by size
+		case "s":
+			// order by size
 
-        case "C":
-            // order by number of items
+		case "C":
+			// order by number of items
 
-        case "a":
-            // toggle disk usage/apparent size
+		case "a":
+			// toggle disk usage/apparent size
 
-        case "M":
-            // order by latest modify time
+		case "M":
+			// order by latest modify time
 
-        case "d":
-            // delete selected file or directory
+		case "d":
+			// delete selected file or directory
 
-        case "t":
-            // toggles directories before files when sorting
+		case "t":
+			// toggles directories before files when sorting
 
-        case "g":
-            // toggles percentage, graph, both, or none
+		case "g":
+			// toggles percentage, graph, both, or none
 
-        case "u":
-            // toggle display of shared/unique size column for directories
+		case "u":
+			// toggle display of shared/unique size column for directories
 
-        case "c":
-            // toggle display of child counts
+		case "c":
+			// toggle display of child counts
 
-        case "m":
-            // toggle display of latest modify time
+		case "m":
+			// toggle display of latest modify time
 
-        case "e":
-            // Show/hide hidden files and directories
+		case "e":
+			// Show/hide hidden files and directories
 
-        case "i":
-            // show information about selected item
+		case "i":
+			// show information about selected item
 
-        case "r":
-            // refresh calculations in the current directory
+		case "r":
+			// refresh calculations in the current directory
 
-        case "b":
-            // spawn shell in current directory
-        }
+		case "b":
+			// spawn shell in current directory
+		}
 
-    case tea.WindowSizeMsg:
-        headerHeight := lipgloss.Height(m.headerView())
-        footerHeight := lipgloss.Height(m.footerView())
-        verticalMarginHeight := headerHeight + footerHeight
+	case tea.WindowSizeMsg:
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight
 
-        if !m.Ready {
-            m.Viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-            m.Viewport.YPosition = headerHeight
-            //m.Viewport.HighPerformanceRendering = useHighPerformanceRenderer
-            m.Viewport.SetContent(m.Content)
-            m.Ready = true
+		if !m.Ready {
+			m.Viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+			m.Viewport.YPosition = headerHeight
+			m.Viewport.HighPerformanceRendering = useHighPerformanceRenderer
+			m.Viewport.SetContent(m.Content)
+			m.Ready = true
 
-            m.Viewport.YPosition = headerHeight + 1
-        } else {
-            m.Viewport.Width = msg.Width
-            m.Viewport.Height = msg.Height - verticalMarginHeight
-        }
-    }
+			m.Viewport.YPosition = headerHeight + 1
+		} else {
+			m.Viewport.Width = msg.Width
+			m.Viewport.Height = msg.Height - verticalMarginHeight
+		}
 
-    m.Viewport, cmd = m.Viewport.Update(msg)
-    cmds = append(cmds,cmd)
+		if useHighPerformanceRenderer {
+			cmds = append(cmds, viewport.Sync(m.Viewport))
+		}
+	}
 
-    return m, tea.Batch(cmds...)
+	m.Viewport, cmd = m.Viewport.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m Model) headerView() string {
-    title := titleStyle.Render(fmt.Sprintf("godu <version>"))
-    line := strings.Repeat("-", max(0, m.Viewport.Width-lipgloss.Width(title)))
-    return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+	title := titleStyle.Render(fmt.Sprintf("godu <version>"))
+	line := strings.Repeat("-", max(0, m.Viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
 }
 
 func (m Model) footerView() string {
-    info := infoStyle.Render(fmt.Sprintf("Total disk usage: %d", m.Subcursor))
-    line := strings.Repeat("-", max(0, m.Viewport.Width-lipgloss.Width(info)))
-    return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+	info := infoStyle.Render(fmt.Sprintf("Total disk usage: %d", m.Subcursor))
+	line := strings.Repeat("-", max(0, m.Viewport.Width-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func (m Model) fileView() string {
+	s := ""
+	for _, file := range m.Files {
+		if file.HighDir == m.CurrentDirectory {
+			m.currentFiles = append(m.currentFiles, file)
+		}
+	}
+
+	m.Content = ""
+	for i, file := range m.currentFiles {
+		cursor := " "
+		if m.Cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %d\t%s\n", cursor, file.Size, file.Name)
+	}
+	s += fmt.Sprintf("\nCursor: %d\n", m.Cursor)
+	for _, file := range m.currentFiles {
+		s += fmt.Sprintln(file)
+	}
+	return s
 }
 
 func max(a, b int) int {
@@ -170,31 +199,10 @@ func max(a, b int) int {
 }
 
 func (m Model) View() string {
-    if !m.Ready {
-        return "\n Initializing..."
-    }
+	if !m.Ready {
+		return "\n Initializing..."
+	}
 
-    for _, file := range m.Files {
-        if file.HighDir == m.CurrentDirectory {
-            m.currentFiles = append(m.currentFiles, file)
-        }
-    }
-
-    m.Content = ""
-    for i, file := range m.currentFiles {
-        cursor := " "
-        if m.Cursor == i {
-            cursor = ">"
-        }
-        m.Content += fmt.Sprintf("%s %d\t%s\n", cursor, file.Size, file.Name)
-    }
-    m.Content += fmt.Sprintf("\nCursor: %d\n", m.Cursor)
-    for _, file := range m.currentFiles {
-        m.Content += fmt.Sprintln(file)
-    }
-    m.Viewport.SetContent(m.Content)
-
-    return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.Viewport.View(), m.footerView())
+	m.Viewport.SetContent(m.fileView())
+	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.Viewport.View(), m.footerView())
 }
-
-
